99c54dbdf62d94109f56f1e6a4f79833cd7062eb
Faridl003 <miftahfaridl2001@gmail.com>
2025-12-03 15:43:41 +0700

    add somethings

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -18,7 +18,12 @@ const Ball = (props) => {
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial color="#fff" flatShading />
+        <meshStandardMaterial
+          color="#fff"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
         <Decal
           map={decal}
           position={[0, 0, 1]}
